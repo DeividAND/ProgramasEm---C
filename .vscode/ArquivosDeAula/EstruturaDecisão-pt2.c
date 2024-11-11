b2fae6f547687ded028065ec82a47878f3696657
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 15:26:30 -0300

    EstruturasdeDecisao-pt2

---
+++ .vscode/ArquivosDeAula/EstruturaDecisão-pt2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    /* Se a 'idade' for 18 ou mais, o programa imprime 'Você é maior de idade'.
+    No nosso exemplo 20 é maior que 18, então a mensagem será exibida */
+
+    int idade = 20;
+
+    if(idade >= 18){
+        printf("Você e maior de idade");
+    }
+}
