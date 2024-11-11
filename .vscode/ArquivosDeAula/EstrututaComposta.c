9a393e27bdc5b738da91144b1773db947aacfd1e
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 16:43:26 -0300

    EstruturaComposta-ex1

---
+++ .vscode/ArquivosDeAula/EstrututaComposta.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    if (condição) {
+        // bloco de código a ser executado se a condição for verdadeira
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+    }
+    */
+}
