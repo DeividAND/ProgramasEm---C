aa89fdfad1ae33f2750c8f3e5bce33a28743f028
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 17:03:15 -0300

    EstruturaComposta-pt4

---
+++ .vscode/ArquivosDeAula/EstruturaComposta-pt4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int nota = 65;
+
+    if (nota >= 60) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("Parabens, voce passou!\n");
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("Infelizmente, voce nao passou.\n");
+    }
+    
+}
