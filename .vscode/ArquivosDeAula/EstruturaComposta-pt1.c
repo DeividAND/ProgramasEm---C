5394229b85d36a0d5dd2b9917119f5e533a026b1
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 16:49:46 -0300

    EstruturaComposta-pt1

---
+++ .vscode/ArquivosDeAula/EstruturaComposta-pt1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int numero1;
+    int numero2;
+
+    numero1 = 10;
+    numero2 = 5;
+
+    if (numero1 > numero2) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("O numero 1 e maior que numero 2\n");
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("O numero 1 e menor que o numero 2\n");
+    }
+    
+}
