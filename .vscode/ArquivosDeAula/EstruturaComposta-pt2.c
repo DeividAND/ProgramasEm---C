08fd5eb0565cca035ec2b9af1087697539c0879d
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 16:54:50 -0300

    EstruturaComposta-pt2

---
+++ .vscode/ArquivosDeAula/EstruturaComposta-pt2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 11;
+
+    if (numero % 2 == 0) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("O numero  e par\n");
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("O numero e impar\n");
+    }
+    
+}
