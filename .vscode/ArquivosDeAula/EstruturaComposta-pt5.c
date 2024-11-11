357792f1332fcd20545c53dead31ed22f2f98c46
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 17:06:30 -0300

    EstruturaComposta-pt5

---
+++ .vscode/ArquivosDeAula/EstruturaComposta-pt5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 16;
+
+    if (idade >= 18) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("Voce e maior de idade.\n");
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("Voce e menor de idade.\n");
+    }
+    
+}
