d6762ed9fd61e3e8c072dfcbc2bb623276a0874b
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-11 16:59:47 -0300

    EstruturaComposta-pt3

---
+++ .vscode/ArquivosDeAula/EstruturaComposta-pt3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    float temperatura = 25.0;
+
+    if (temperatura > 30) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("Esta calor\n");
+    } else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("Nao esta calor\n");
+    }
+    
+}
