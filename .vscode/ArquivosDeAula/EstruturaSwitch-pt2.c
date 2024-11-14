ddd96eadbbfd0fd5168202069badc3c40efc1e51
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-14 10:05:25 -0300

    EstruturaSwitch-pt2

---
+++ .vscode/ArquivosDeAula/EstruturaSwitch-pt2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int opcao;
+    float saldo = 1000.00;
+
+    printf("Escolha uma opcao:\n");
+    printf("1. Verificar saldo\n");
+    printf("2. Fazer deposito\n");
+    printf("3. Fazer saque\n");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+    case 1:
+        printf("O seu saldo e: R$ %.2f\n", saldo);
+    break;
+    case 2:
+        printf("Digite o banco que voce deseja depositar\n");
+        printf("Digite a agencia que voce deseja depositar\n");
+        printf("Digite a conta que voce deseja depositar\n");
+    break;
+    case 3:
+        printf("Digite o valor a sacar: \n");
+    break;
+    default:
+        printf("Opcao invalida! Porfavor tentar novamente.\n");
+    }
+}
