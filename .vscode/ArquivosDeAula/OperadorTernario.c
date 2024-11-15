b074a0053c5ee329f47ae585990520ab07543714
Deivid Augusto Silva <agunegocios@hotmail.com>
2024-11-14 23:02:09 -0300

    OperadorTernario-pt1

---
+++ .vscode/ArquivosDeAula/OperadorTernario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 18;
+    int resultado;
+
+    // Operador Ternário 
+    // Condição ex idade > 20 ? código caso a condição seja verdadeira (ex: printf("verdadeira")) : código caso a condição seja falsa (ex: printf("falsa"))
+
+    idade >= 18 ? printf("Voce e maior de idade\n") : printf("Voce e menor de idade\n");
+}
